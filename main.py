7933f133362b79668f1e55eb0063499d54d6f061
Myers Jacob <813862@stma.k12.mn.us>
2021-03-31 10:16:11 -0500

    remember not to delete random things

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#WRITE YOUR CODE IN THIS FILE
+def close10(x, y):
+
+    if abs(x-10) > abs(y-10):
+        return y
+
+    elif abs(x-10) < abs(y-10):
+        return x
+
+    else:
+        return 0
+        
+print (close10(7, 13))
